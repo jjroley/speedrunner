3ef0b51f199e2bf577d10ae38fae4df5a1075d50
Jonathan Roley <jonathan.roley@gmail.com>
2024-02-08 18:31:36 -0500

    convert player & entity to classes

--- src/entity.js
+++ src/entity.js
@@ -3,166 +3,169 @@
  * 
  * Base class for a single physics-bound entity.
  */
-const Entity = (function() {
-	function Entity(x, y, w, h) {
-			this.x = x || 0
-			this.y = y || 0
-			this.w = w || BLOCK_SIZE
-			this.h = h || BLOCK_SIZE
-			
-			this.xVel = 0.2
-			this.yVel = 0
-			this.prevXVel = this.xVel
-			this.prevYVel = this.yVel
-			
-			this.prevX = this.x
-			this.prevY = this.y
-			
-			this.collisions = {}
-			
-			this.groundObject = null
-			this.friction = 1
+
+class Entity {
+	constructor(x, y, w, h) {
+		this.x = x || 0
+		this.y = y || 0
+		this.w = w || BLOCK_SIZE
+		this.h = h || BLOCK_SIZE
+
+		this.xVel = 0.2
+		this.yVel = 0
+		this.prevXVel = this.xVel
+		this.prevYVel = this.yVel
+
+		this.prevX = this.x
+		this.prevY = this.y
+
+		this.collisions = {}
+
+		this.groundObject = null
+		this.friction = 1
 	}
-	Entity.prototype.beforeCollision = function() {
-			this.yVel += GRAVITY
-			
-			// unset collisions
-			this.collisions.top = false
-			this.collisions.right = false
-			this.collisions.left = false
-			this.collisions.bottom = false
-			this.groundObject = null
-			
-			this.prevXVel = this.xVel
-			this.prevYVel = this.yVel
-			this.prevX = this.x
-			this.prevY = this.y
-			this.x += this.xVel
-			this.y += this.yVel
-	}
-	Entity.prototype.update = function() {
-			// if on moving object, apply frictional force
-			if(this.groundObject) {
-					if(this.groundObject.xVel !== undefined) {
-							this.xVel += (this.groundObject.xVel - this.xVel) * this.groundObject.friction
-					}
-			}
-	}
-	Entity.prototype.resolveComplexCollision = function(obj) {
-			const vertices = obj.vertices
 
+	beforeCollision() {
+		this.yVel += GRAVITY
 
-			const vec = getCollisionResolutionVector(this, vertices)
+		// unset collisions
+		this.collisions.top = false
+		this.collisions.right = false
+		this.collisions.left = false
+		this.collisions.bottom = false
+		this.groundObject = null
 
-			if(vec) {
-					// this.x += vec.x
-					// this.y += vec.y
-
-					this.xVel += vec.x
-					this.yVel += vec.y
-			}
-
-
-
-			// if(obj.tile === '^') {
-			//     const offset = (this.x + this.w / 2) - (obj.x + obj.w / 2)
-			//     const absoluteOffset = Math.abs(offset) - this.w / 2
-
-			//     if(absoluteOffset <= 0 || this.prevY > obj.y + obj.h) {
-			//         if(this.y + this.h * 0.5 < obj.y + obj.h * 0.5) {
-			//             this.y = obj.y - this.h
-			//             this.yVel = 0
-			//             this.collisions.bottom = true
-			//             this.groundObject = obj
-			//         } else {
-			//             this.y = obj.y + obj.h
-			//             this.yVel = 0
-			//             this.collisions.top = true
-			//         }
-			//     } else {
-			//         const penetrationY = this.y + this.h - obj.y - absoluteOffset * 2
-			//         const penetrationX = penetrationY / 2
-			//         if(penetrationY > 0 && this.prevY + this.h <= obj.y + obj.h) {
-			//             const newX = (this.x - this.prevX)
-
-			//             this.yVel = 0
-									
-			//             this.y -= penetrationY * 0.5
-			//             if(this.x !== this.prevX) {
-			//                 this.x -= (offset > 0 ? -penetrationX : penetrationX) * 0.5
-			//             }
-
-			//             this.collisions.bottom = true
-			//             this.groundObject = obj
-			//         }
-			//     }
-					
-			//     if(this.prevY + this.h > obj.y + obj.h) {
-			//         if(this.prevXVel) {
-			//             // this.x = this.prevXVel > 0 ? obj.x - this.w : obj.x + obj.w
-			//         }
-			//     }
-			// }
-
-			// if(obj.tile === 'R') {
-
-			// }
-	}
-	Entity.prototype.resolveYCollision = function(obj) {
-			this.yVel = 0
-			this.collisions.top = this.prevYVel < 0
-			this.collisions.bottom = this.prevYVel >= 0
-			this.y = this.prevYVel >= 0 ? obj.y - this.h : obj.y + obj.h
-			
-			if(this.prevYVel >= 0) {
-					this.groundObject = obj
-			}
-	}
-	Entity.prototype.resolveXCollision = function(obj) {
-			this.xVel = 0
-			this.collisions.right = this.prevXVel > 0
-			this.collisions.left = this.prevXVel < 0
-			if(this.prevXVel !== 0) {
-					this.x = this.prevXVel > 0 ? obj.x - this.w : obj.x + obj.w
-			}
-	}
-	Entity.prototype.resolveCollisions = function(collidingObjects) {
-			// sort collisions by overlap area this is a hacky(ish)
-			// way to make collision resolutions are less wacky
-			collidingObjects.sort(sortByOverlapArea(this))
-			
-			for(var i = 0; i < collidingObjects.length; i++) {
-					const obj = collidingObjects[i];
-					
-					// cases where we're touching the object but have already applied collision
-					if(this.x + this.w <= obj.x) {
-							this.collisions.right = true
-							continue;
-					}
-					if(this.x >= obj.x + obj.w) {
-							this.collisions.left = true
-							continue;
-					}
-					if(this.y + this.h <= obj.y) {
-							this.collisions.bottom = true
-							this.groundObject = obj
-							continue;
-					}
-					if(this.y >= obj.y + obj.h) {
-							this.collisions.top = true
-							continue;
-					}
-					
-					// apply collisions
-					if(obj.vertices) {
-							this.resolveComplexCollision(obj)
-					}else if(this.prevX + this.w <= obj.x || this.prevX >= obj.x + obj.w) {
-							this.resolveXCollision(obj)
-					} else if(this.prevY + this.h <= obj.y || this.prevY >= obj.y + obj.h) {
-							this.resolveYCollision(obj)
-					}
-			}
+		this.prevXVel = this.xVel
+		this.prevYVel = this.yVel
+		this.prevX = this.x
+		this.prevY = this.y
+		this.x += this.xVel
+		this.y += this.yVel
 	}
 	
-	return Entity
-})()
+	update() {
+		// if on moving object, apply frictional force
+		if (this.groundObject) {
+			if (this.groundObject.xVel !== undefined) {
+				this.xVel += (this.groundObject.xVel - this.xVel) * this.groundObject.friction
+			}
+		}
+	}
+
+	resolveComplexCollision(obj) {
+		const vertices = obj.vertices
+
+		const vec = getCollisionResolutionVector(this, vertices)
+
+		if (vec) {
+			// this.x += vec.x
+			// this.y += vec.y
+
+			this.xVel += vec.x
+			this.yVel += vec.y
+		}
+
+		// if(obj.tile === '^') {
+		//     const offset = (this.x + this.w / 2) - (obj.x + obj.w / 2)
+		//     const absoluteOffset = Math.abs(offset) - this.w / 2
+
+		//     if(absoluteOffset <= 0 || this.prevY > obj.y + obj.h) {
+		//         if(this.y + this.h * 0.5 < obj.y + obj.h * 0.5) {
+		//             this.y = obj.y - this.h
+		//             this.yVel = 0
+		//             this.collisions.bottom = true
+		//             this.groundObject = obj
+		//         } else {
+		//             this.y = obj.y + obj.h
+		//             this.yVel = 0
+		//             this.collisions.top = true
+		//         }
+		//     } else {
+		//         const penetrationY = this.y + this.h - obj.y - absoluteOffset * 2
+		//         const penetrationX = penetrationY / 2
+		//         if(penetrationY > 0 && this.prevY + this.h <= obj.y + obj.h) {
+		//             const newX = (this.x - this.prevX)
+
+		//             this.yVel = 0
+
+		//             this.y -= penetrationY * 0.5
+		//             if(this.x !== this.prevX) {
+		//                 this.x -= (offset > 0 ? -penetrationX : penetrationX) * 0.5
+		//             }
+
+		//             this.collisions.bottom = true
+		//             this.groundObject = obj
+		//         }
+		//     }
+
+		//     if(this.prevY + this.h > obj.y + obj.h) {
+		//         if(this.prevXVel) {
+		//             // this.x = this.prevXVel > 0 ? obj.x - this.w : obj.x + obj.w
+		//         }
+		//     }
+		// }
+
+		// if(obj.tile === 'R') {
+
+		// }
+	}
+
+	resolveYCollision(obj) {
+		this.yVel = 0
+		this.collisions.top = this.prevYVel < 0
+		this.collisions.bottom = this.prevYVel >= 0
+		this.y = this.prevYVel >= 0 ? obj.y - this.h : obj.y + obj.h
+
+		if (this.prevYVel >= 0) {
+			this.groundObject = obj
+		}
+	}
+
+	resolveXCollision(obj) {
+		this.xVel = 0
+		this.collisions.right = this.prevXVel > 0
+		this.collisions.left = this.prevXVel < 0
+		if (this.prevXVel !== 0) {
+			this.x = this.prevXVel > 0 ? obj.x - this.w : obj.x + obj.w
+		}
+	}
+
+	resolveCollisions(collidingObjects) {
+		// sort collisions by overlap area this is a hacky(ish)
+		// way to make collision resolutions are less wacky
+		collidingObjects.sort(sortByOverlapArea(this))
+
+		for (var i = 0; i < collidingObjects.length; i++) {
+			const obj = collidingObjects[i];
+
+			// cases where we're touching the object but have already applied collision
+			if (this.x + this.w <= obj.x) {
+				this.collisions.right = true
+				continue;
+			}
+			if (this.x >= obj.x + obj.w) {
+				this.collisions.left = true
+				continue;
+			}
+			if (this.y + this.h <= obj.y) {
+				this.collisions.bottom = true
+				this.groundObject = obj
+				continue;
+			}
+			if (this.y >= obj.y + obj.h) {
+				this.collisions.top = true
+				continue;
+			}
+
+			// apply collisions
+			if (obj.vertices) {
+				this.resolveComplexCollision(obj)
+			} else if (this.prevX + this.w <= obj.x || this.prevX >= obj.x + obj.w) {
+				this.resolveXCollision(obj)
+			} else if (this.prevY + this.h <= obj.y || this.prevY >= obj.y + obj.h) {
+				this.resolveYCollision(obj)
+			}
+		}
+	}
+}
+
--- src/player.js
+++ src/player.js
@@ -3,192 +3,195 @@
  *
  * Handles keyboard control of the player and special abilities
  * not found in normal entities
- * 
- * @extends Entity
  */
-const Player = (function() {
-	function Player(x, y, w, h) {
-			this.canJump = false
-			this.direction = 1 // last moved direction
-			this.r = 0
-			
-			this.defaultSpeed = 2.5
-			this.speed = this.defaultSpeed
-			
-			this.jumpTimer = 0
-			this.jumpLevel = 0
-			this.jumpLevelTimer = 0
-			this.highJumpTimer = 0
-			this.highJumpDuration = 25
-			this.wasOnGround = false
-	
-			this.sprintTimer = 0
-			this.sprintDuration = 20
-			this.sprintCooldown = 300
-			this.sprintSpeed = 7
-			this.trail = []
-			
+
+class Player extends Entity {
+	constructor(x, y, w, h) {
+		super(x, y, w, h)
+
+		this.canJump = false
+		this.direction = 1 // last moved direction
+		this.r = 0
+
+		this.defaultSpeed = 2.5
+		this.speed = this.defaultSpeed
+
+		this.jumpTimer = 0
+		this.jumpLevel = 0
+		this.jumpLevelTimer = 0
+		this.highJumpTimer = 0
+		this.highJumpDuration = 25
+		this.wasOnGround = false
+
+		this.sprintTimer = 0
+		this.sprintDuration = 20
+		this.sprintCooldown = 300
+		this.sprintSpeed = 7
+		this.trail = []
+
+		this.groundPound = false
+
+		this.dead = false
+		this.respawnTimer = 0
+		this.respawnDuration = 100
+		this.respawnPoint = { x, y }
+	}
+
+	beforeCollision() {
+		if (!this.dead) {
+			super.beforeCollision()
+		}
+	}
+
+	update() {
+		if (this.dead) {
+			this.respawnTimer--;
+			if (this.respawnTimer <= 0) {
+				this.respawn()
+			}
+			return;
+		}
+
+		// call Entity initial update
+		super.update()
+
+		// key controlled movement
+		if (keys.pressed('left')) {
+			this.xVel = -this.speed
+			this.direction = -1
+		}
+		else if (keys.pressed('right')) {
+			this.xVel = this.speed
+			this.direction = 1
+		} else {
+			this.xVel = 0
+		}
+
+		// jumping
+		if (!this.wasOnGround && !keys.pressed('jump') && this.collisions.bottom) {
+			this.jumpLevelTimer = 15
+		}
+
+		// countdown various jump timers ;)
+		this.jumpTimer = Math.max(0, this.jumpTimer - 1)
+		this.jumpLevelTimer = Math.max(0, this.jumpLevelTimer - 1)
+		this.highJumpTimer = Math.max(0, this.highJumpTimer - 1)
+
+		// saves state of bottom collision
+		this.wasOnGround = this.collisions.bottom
+
+		if (this.collisions.bottom && keys.pressed('jump')) {
+			if (this.jumpLevelTimer > 0) {
+				this.jumpLevel++
+			} else {
+				this.jumpLevel = 0
+			}
+
+			this.jumpTimer = 15
+			this.yVel = -(4 + this.jumpLevel * 0.6)
+
+			if (this.jumpLevel >= 3) {
+				this.highJumpTimer = this.highJumpDuration
+				this.jumpLevel = 0
+			}
+		}
+
+		// extra height if key is held down
+		if (keys.pressed('jump') && this.jumpTimer > 0) {
+			this.yVel -= 0.2
+		}
+
+		// high jump
+		if (this.highJumpTimer > 0) {
+			this.yVel -= 0.03
+		}
+
+		// ground pounding
+		if (this.collisions.bottom) {
+			if (this.groundPound) {
+
+			}
 			this.groundPound = false
+		} else {
+			if (keys.pressStart('ground_pound') && !this.groundPound) {
+				this.groundPound = true
+			}
+		}
 
-			this.dead = false
-			this.respawnTimer = 0
-			this.respawnDuration = 100
-			this.respawnPoint = { x, y }
-			
-			Entity.call(this, x + 5, y + 5, w - 10, h - 10); // inherit from entity
-	}
-	Player.prototype = Object.create(Entity.prototype);
-	Player.prototype.beforeCollision = function() {
-			if(!this.dead) {
-					Entity.prototype.beforeCollision.call(this)
-			}
-	}
-	Player.prototype.update = function() {
-			if(this.dead) {
-					this.respawnTimer--;
-					if(this.respawnTimer <= 0) {
-							this.respawn()
-					}
-					return;
-			}
+		// ground pound has constant y velocity
+		if (this.groundPound) {
+			this.yVel = 13
+		}
 
-			// call Entity initial update
-			Entity.prototype.update.call(this)
-			
-			// key controlled movement
-			if(keys.pressed('left')) {
-					this.xVel = -this.speed
-					this.direction = -1
+		// sprinting
+		let sprinting = false;
+
+		this.sprintTimer = Math.max(0, this.sprintTimer - 1)
+
+		if (this.sprintTimer === 0 && keys.pressed('sprint')) {
+			this.sprintTimer = this.sprintCooldown
+			if (!this.collisions.bottom) {
+				this.yVel *= (1 + Math.sqrt(Math.abs(this.yVel)) / 8)
 			}
-			else if(keys.pressed('right')) {
-					this.xVel = this.speed
-					this.direction = 1
-			} else {
-					this.xVel = 0
+		}
+
+		if (this.sprintTimer > this.sprintCooldown - this.sprintDuration) {
+			this.speed = this.sprintSpeed
+			this.xVel = this.speed * this.direction
+			sprinting = true
+
+		} else {
+			this.speed = this.defaultSpeed
+		}
+
+		// rotation
+		if (this.sprintTimer > this.sprintCooldown - this.sprintDuration) {
+			this.r = (this.sprintCooldown - this.sprintTimer) / this.sprintDuration * 360 * this.direction
+		} else if (this.highJumpTimer > 0) {
+			this.r = (this.highJumpDuration - this.highJumpTimer) / this.highJumpDuration * 360 * this.direction
+		} else {
+			this.r = 0
+		}
+
+		// trail
+		if ((this.groundPound || sprinting || this.highJumpTimer > 0)) {
+			this.trail.push([this.x, this.y, this.r])
+			if (this.trail.length > 20) {
+				this.trail.shift()
 			}
-			
-			// jumping
-			if(!this.wasOnGround && !keys.pressed('jump') && this.collisions.bottom) {
-					this.jumpLevelTimer = 15
-			}
-			
-			// countdown various jump timers ;)
-			this.jumpTimer = Math.max(0, this.jumpTimer - 1)
-			this.jumpLevelTimer = Math.max(0, this.jumpLevelTimer - 1)
-			this.highJumpTimer = Math.max(0, this.highJumpTimer - 1)
-			
-			// saves state of bottom collision
-			this.wasOnGround = this.collisions.bottom
-			
-			if(this.collisions.bottom && keys.pressed('jump')) {
-					if(this.jumpLevelTimer > 0) {
-							this.jumpLevel++
-					} else {
-							this.jumpLevel = 0
-					}
-					
-					this.jumpTimer = 15
-					this.yVel = -(4 + this.jumpLevel * 0.6)
-					
-					if(this.jumpLevel >= 3) {
-							this.highJumpTimer = this.highJumpDuration
-							this.jumpLevel = 0
-					}
-			}
-			
-			// extra height if key is held down
-			if(keys.pressed('jump') && this.jumpTimer > 0) {
-					this.yVel -= 0.2
-			}
-			
-			// high jump
-			if(this.highJumpTimer > 0) {
-					this.yVel -= 0.03
-			}
-			
-			// ground pounding
-			if(this.collisions.bottom) {
-					if(this.groundPound) {
-							
-					}
-					this.groundPound = false
-			} else {
-					if(keys.pressStart('ground_pound') && !this.groundPound) {
-							this.groundPound = true
-					}
-			}
-			
-			// ground pound has constant y velocity
-			if(this.groundPound) {
-					this.yVel = 13
-			}
-			
-			// sprinting
-			let sprinting = false;
-			
-			this.sprintTimer = Math.max(0, this.sprintTimer - 1)
-			
-			if(this.sprintTimer === 0 && keys.pressed('sprint')) {
-					this.sprintTimer = this.sprintCooldown
-					if(!this.collisions.bottom) {
-							this.yVel *= (1 + Math.sqrt(Math.abs(this.yVel)) / 8)
-					}
-			}
-			
-			if(this.sprintTimer > this.sprintCooldown - this.sprintDuration) {
-					this.speed = this.sprintSpeed
-					this.xVel = this.speed * this.direction
-					sprinting = true
-	
-			} else {
-					this.speed = this.defaultSpeed
-			}
-			
-			// rotation
-			if(this.sprintTimer > this.sprintCooldown - this.sprintDuration) {
-					this.r = (this.sprintCooldown - this.sprintTimer) / this.sprintDuration * 360 * this.direction
-			} else if(this.highJumpTimer > 0) {
-					this.r = (this.highJumpDuration - this.highJumpTimer) / this.highJumpDuration * 360 * this.direction
-			} else {
-					this.r = 0
-			}
-			
-			// trail
-			if((this.groundPound || sprinting || this.highJumpTimer > 0)) {
-					this.trail.push([this.x, this.y, this.r])
-					if(this.trail.length > 20) {
-							this.trail.shift()
-					}
-			} else {
-					this.trail.shift()
-			}
+		} else {
+			this.trail.shift()
+		}
 	}
-	Player.prototype.display = function() {
-			for(let i = 0; i < this.trail.length; i++) {
-					const p = this.trail[i]
-					push();
-					translate(p[0] + this.w / 2, p[1] + this.h / 2);
-					rotate(p[2]);
-					fill(113, 237, 78, i * (255 / (this.trail.length * 2)));
-					rect(-this.w / 2, -this.h / 2, this.w, this.h);
-					pop();
-			}    
-	
+
+	display() {
+		for (let i = 0; i < this.trail.length; i++) {
+			const p = this.trail[i]
 			push();
-			translate(this.x + this.w / 2, this.y + this.h / 2);
-			rotate(this.r);
-			fill(113, 237, 78);
+			translate(p[0] + this.w / 2, p[1] + this.h / 2);
+			rotate(p[2]);
+			fill(113, 237, 78, i * (255 / (this.trail.length * 2)));
 			rect(-this.w / 2, -this.h / 2, this.w, this.h);
 			pop();
+		}
+
+		push();
+		translate(this.x + this.w / 2, this.y + this.h / 2);
+		rotate(this.r);
+		fill(113, 237, 78);
+		rect(-this.w / 2, -this.h / 2, this.w, this.h);
+		pop();
 	}
-	Player.prototype.kill = function() {
-			this.dead = true
-			this.respawnTimer = this.respawnDuration
+
+	kill() {
+		this.dead = true
+		this.respawnTimer = this.respawnDuration
 	}
-	Player.prototype.respawn = function() {
-			this.x = this.prevX = this.respawnPoint.x
-			this.y = this.prevY = this.respawnPoint.y
-			this.dead = false
+
+	respawn() {
+		this.x = this.prevX = this.respawnPoint.x
+		this.y = this.prevY = this.respawnPoint.y
+		this.dead = false
 	}
-	return Player 
-})()
+}
+
